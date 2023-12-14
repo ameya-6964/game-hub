af68a62c9c35414a1031a5723796b6d048ed4af7
ameya-6964 <belvalkar7@gmail.com>
2023-12-15 02:30:33 +0530

    Showing Spinner While Fetching Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,27 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
-
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) {
+    return (
+      <VStack flex="flex" flexDirection={"column"}>
+        {data.map((genre) => (
+          <Spinner />
+        ))}
+      </VStack>
+    );
+  }
+  if (error) return null;
   return (
     <List>
       {data.map((genre) => (
